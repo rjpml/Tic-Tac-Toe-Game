0e44fc4ef0b5d24d8894dadd9dcfa38caad1ce95
Rey John Paul M. Lansangan <rj_lansangan@yahoo.com>
2025-06-19 17:06:26 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,102 +1,37 @@
-# ----- Global Variables -----
+import tkinter #To create graphical user interface (GUIs)
 
-#Game Board
-board = ["-", "-", "-",
-         "-", "-", "-",
-         "-", "-", "-",]
+#Game setup
+player_X = "X"
+player_O = "O"
+current_player = player_X
 
-#If game is still going
-game_still_going = True
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
 
-#Who won? or Tie?
-winner = None
+color_blue = "#4584b4"
+color_yellow = "#ffde57"
+color_gray = "#343434"
+color_light_gray = "#646464"
 
-#Who turn is it?
-current_player = "X"
+#Window Setup
+window = tkinter.Tk() #Create the game window
+window.title("Tic Tac Toe")
+window.resizable(False, False) #Disable window resizing (Both Horizontal & Vertical)
 
-def display_board():
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print(board[6] + " | " + board[7] + " | " + board[8])
+frame = tkinter.Frame(window) #Create a frame widget inside the main window to organize other widgets
 
-#Play a game of tic tac toe
-def play_game():
+#Create a label widget inside the frame
+label = tkinter.Label(frame, text = current_player + "'s turn", font = ("Consalas", 20), background = color_gray,
+                      foreground = "white") #Display the current player's turn with modified font & background
 
-    display_board()
+for row in range(3):
+    for column in range(3):
+        board[row][column] = tkinter.Button(frame, text = "", font = ("Consales", 50, "bold"),
+                                                                      background = color_gray, foreground = color_blue, 
+                                                                      width = 4, height = 1)
 
-    #While the game is still going
-    while game_still_going:
-        
-        #Handle single turn of an arbitrary player
-        handle_turn(current_player)
+label.grid(row = 0, column = 0)
+frame.pack() #Add the frame to the window using the pack layout manager
 
-        #Check if the game has ended
-        check_if_game_over()
-
-        #Flip to the other player
-        flip_player()
-
-    #The game has ended
-    if winner == "X" or winner == "O":
-        print(winner + " won.")
-    elif winner == None:
-        print("Tie.")
-
-#Handle single turn of an arbitrary player
-def handle_turn(player):
-    position = input("Choose a position from 1-9: ")
-
-    #Convert the input from string to integer,
-    #then subtract 1 to match Python's 0-based index
-    #(User thinks in 1–9, but Python uses 0–8)
-    #We only subtract 1 for the code's logic,
-    #because Python list indexing starts at 0, not 1.
-    position = int(position) - 1
-
-    board[position] = "X"
-
-    display_board()
-
-def check_if_game_over():
-    check_for_winner()
-    check_if_tie()
-
-def check_for_winner():
-
-    #Setup global variable
-    global winner
-
-    #Check rows
-    row_winner = check_row()
-    #Check column
-    column_winner = check_column()
-    #Check diagonals
-    diagonal_winner = check_diagonals()
-
-    #Get the winner
-    if row_winner:
-        winner = row_winner()
-    elif column_winner:
-        winner = column_winner()
-    elif diagonal_winner:
-        winner = diagonal_winner()
-    else:
-        winner = None
-    return
-
-def check_row():
-    return
-
-def check_column():
-    return
-
-def check_diagonals():
-    return
-
-def check_if_tie():
-    return
-
-def flip_player():
-    return
-
-play_game()
+window.mainloop() #Keeps the window open until the user closes it
